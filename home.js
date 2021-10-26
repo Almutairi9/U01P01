67d254ab8f2edd1e36ee3a0c3e015c529be7530e
Rawan Almutairi <rawan.fahad@tuwaiq.edu.sa>
2021-10-26 17:02:51 +0300

    up

--- home.js
+++ home.js
@@ -3,79 +3,77 @@ const learning = [
     teacherName: "Rawan Almutairi",
     teacherImage: "user.png",
     cousreName: "Information Tecnology",
-    cousreImage: "It.jpg",
-    cousrebackgroundImg: "itimg.png",
+    cousreImage: "itimg.jpg",
+    cousrebackgroundImg: "itimg.jpg",
     cousredescriptionForSubject:
-      "The field of information technology, or IT, covers the support, administration, and design of telecommunications and computer systems. Some positions in this field include system analysts, software programmers, computer scientists, computer support specialists, and network and database administrators.",
-    cousrebackgroundSecondImg: "",
-    cousredescriptionForSecondSubject: "",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
-  },
+      "The field of information technology, or IT, covers the support, administration, and design of telecommunications and computer systems. Some positions in this field include system analysts, software programmers, computer scientists, computer support specialists, and network and database administrators.", 
+    cousredescriptionForSecondSubject: "", 
+    url: "https://www.youtube.com/embed/gkOdIZwUKfM",
+    secoundurl: "https://www.youtube.com/embed/bSsnjFzaNK8",
+  }, 
   {
-    teacherName: "Rawan Almutairi",
+    teacherName: "Suha Alhomaid",
     teacherImage: "user.png",
     cousreName: "Math",
-    cousreImage: "It.jpg",
-    cousrebackgroundImg: "itimg.png",
+    cousreImage: "mathimg.jpg", 
+    cousrebackgroundImg: "mathimg.jpg",
     cousredescriptionForSubject:
       " Mathematics is the science that deals with the logic of shape, quantity, and arrangement. Math is all around us, in everything we do. It is the building block for everything in our daily lives, including mobile devices, architecture (ancient and modern), art, money, engineering, and even sports ",
-    cousrebackgroundSecondImg: "",
     cousredescriptionForSecondSubject: "",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
+    url: "https://www.youtube.com/embed/TMubSggUOVE",
+    secoundurl: "https://www.youtube.com/embed/grnP3mduZkM",
   },
   {
     teacherName: "Rawan Almutairi",
     teacherImage: "user.png",
     cousreName: "Art",
-    cousreImage: "It.jpg",
-    cousrebackgroundImg: "itimg.png",
+    cousreImage: "artimg.jpg",
+    cousrebackgroundImg: "artimg.jpg",
     cousredescriptionForSubject:
       " Introduction to Art: Concepts & Techniques is an art appreciation course created for individuals without any artistic background. ... The main emphasis of the course is to teach you hands-on studio art techniques as you conduct personal research and explore your own creativity.",
-    cousrebackgroundSecondImg: "",
+    // cousrebackgroundSecondImg: "artimg.jpg",
     cousredescriptionForSecondSubject: "",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
+    url: "https://www.youtube.com/embed/gaTLt9HKzmk",
+    secoundurl: "https://www.youtube.com/embed/gaTLt9HKzmk",
   },
   {
     teacherName: "Rawan Almutairi",
     teacherImage: "user.png",
     cousreName: "Biology  ",
-    cousreImage: "It.jpg",
-    cousrebackgroundImg: "itimg.png",
+    cousreImage: "bioolgyimg.jpg",
+    cousrebackgroundImg: "bioolgyimg.jpg",
     cousredescriptionForSubject:
       "Biology is the study of living things. It encompasses the cellular basis of living things, the energy metabolism that underlies the activities of life, and the genetic basis for inheritance in organisms. ... Types of practical biology include plant breeding, wildlife management, medical science, and crop production.",
-    cousrebackgroundSecondImg: "",
+    // cousrebackgroundSecondImg: "bioolgyimg.jpg",
     cousredescriptionForSecondSubject: "",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
+    url: "https://www.youtube.com/embed/Q8ijKtdIRMQ",
+    secoundurl: "https://www.youtube.com/embed/-tJmEIU1RgQ",
   },
   {
     teacherName: "Rawan Almutairi",
     teacherImage: "user.png",
     cousreName: "Chemistry",
-    cousreImage: "It.jpg",
-    cousrebackgroundImg: "itimg.png",
+    cousreImage: "chemistryimg.jpg",
+    cousrebackgroundImg: "chemistryimg.jpg",
     cousredescriptionForSubject:
       " Chemistry is the study of matter and the chemical reactions between substances. Chemistry is also the study of matter's composition, structure, and properties. Chemistry is sometimes called “the central science,” because it bridges physics with other natural sciences, such as geology and biology.",
-    cousrebackgroundSecondImg: "",
+    // cousrebackgroundSecondImg: "chemistryimg.jpg",
     cousredescriptionForSecondSubject: "",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
+    url: "https://www.youtube.com/embed/Rd4a1X3B61w",
+    secoundurl: "https://www.youtube.com/embed/6Q-pYtR5I90",
   },
   {
     teacherName: "Rawan Almutairi",
     teacherImage: "user.png",
     cousreName: "English language ",
-    cousreImage: "It.jpg",
-    cousrebackgroundImg: "itimg.png",
+    cousreImage: "englishimg.jpg",
+    cousrebackgroundImg: "englishimg.jpg",
     cousredescriptionForSubject:
       " English is thought to be one of the most important languages in the world. There are many reasons why English is so important. One of the reasons is that English is spoken as the first language in many countries. There are 104 countries where English is spoken as the first language.",
-    cousrebackgroundSecondImg: "",
+    cousrebackgroundSecondImg: "englishimg.jpg",
     cousredescriptionForSecondSubject: "",
-    url: "https://www.youtube.com/embed/o-cKmAeNJiA",
-    secoundurl: "https://www.youtube.com/embed/o-cKmAeNJiA",
+    url: "https://www.youtube.com/embed/WlNiUQwqflE",
+    secoundurl: "https://www.youtube.com/embed/WlNiUQwqflE",
   },
 ];
 
@@ -85,7 +83,7 @@ const render = () => {
     $(".cards").append(` <div class ="card">
         <div class='divSubImg'><img  src= "${learning[index].cousreImage}" alt="IT-Subject"/></div>   
         <p class ="par"> ${learning[index].cousreName} </p>
-        <button class = "btn" id ="btn${index}"> Enroll </button>
+        <button class = "btn success" id ="btn${index}"> Enroll </button>
          </div>`);
     $(`#btn${index}`).click(() => seeMore(index));
     // still we have to put vote faivorate **
@@ -98,6 +96,7 @@ render();
 function seeMore(index) {
   $(".videoPro").hide();
   $(".cards").hide();
+  $(".introduction").hide();
   console.log(learning[index]);
   const item = learning[index];
   $(".oneitem").append(` <div class ="secondLevelPage">
@@ -123,25 +122,21 @@ function secondLevel(index) {
    <button class = "Submit" id ="Submit${index}"> Take quiz </button> </div> `);
   $(`#NextLecture${index}`).off("click");
   render();
-} 
+}
 
-  const seeTecjers = () => {
-  // function seeTecjers(index) { 
+const seeTecjers = () => {
+
   $(".Teachers").html("");
-  //  console.log("seeTecjers");
-   $(".cards").hide();
-   $(".videoPro").hide();
-  for (let index = 0; index < learning.length; index++) { 
-  console.log(learning[index]);
-  const item = learning[index];
+  $(".cards").hide();
+  $(".videoPro").hide();
+  for (let index = 0; index < learning.length; index++) {
+    console.log(learning[index]);
+    const item = learning[index];
     $(".Teachers").append(`<div class="teacher">
     <p class ="par">${learning[index].teacherName} </p> 
     <div class='divTeacherImg'><img src="${learning[index].teacherImage}" alt="teacher-image"/></div>  
-    </div>`); 
-    $(`#Teacherbtn${index}`).click(() => seeTecjers(index)); }
-    
-};
- 
-//seeTecjers();
-
-// $(`#Teacherbtn`).click(() => seeTecjers());
+    </div>`);
+    $(`#Teacherbtn${index}`).click(() => seeTecjers(index));
+  }
+}; 
+//seeTecjers(); إذا حيتها داخل الاقواس تطلع recution function too much loading ...
